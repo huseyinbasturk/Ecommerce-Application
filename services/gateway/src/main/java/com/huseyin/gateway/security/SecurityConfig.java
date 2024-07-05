57025d1628b107392ff755e7092da4f40aa43ffa
huseyinbasturk <hb.basturk41@gmail.com>
2024-07-05 08:55:06 +0200

    finalizing

---
+++ services/gateway/src/main/java/com/huseyin/gateway/security/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.huseyin.gateway.security;public class SecurityConfig {
+}
--- services/order/src/main/java/com/huseyin/ecommerce/config/KafkaOrderTopicConfig.java
+++ services/order/src/main/java/com/huseyin/ecommerce/config/KafkaOrderTopicConfig.java
@@ -3,7 +3,7 @@ package com.huseyin.ecommerce.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaOrderTopicConfig {
